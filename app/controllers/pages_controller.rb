c59af8e0b36d2da2861cb6a99d4c4822337566ea
Katy Pedelty <katy.ryan.pedelty@gmail.com>
2016-05-21 17:46:32 -0700

    Show current time on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    @time_to_display = Time.now.strftime("%I:%M%P")
   end
 end
--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -6,5 +6,10 @@ describe 'unauthenticated pages' do
       visit '/index'
       expect(page.text).to include "Hello World"
     end
+
+    it 'provides the date and time' do
+      visit '/index'
+      expect(page.text).to match /The current time is [0-9]{2}:[0-9]{2}[ap]m./
+    end
   end
 end
