af365b292afae0a8e5409c89e8692ee57bcdf971
Katy Pedelty <katy.ryan.pedelty@gmail.com>
2016-05-21 17:51:57 -0700

    Add 5 paragraphs to the home page
    Okay, this is really just for example. So is the next commit.

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -11,5 +11,14 @@ describe 'unauthenticated pages' do
       visit '/index'
       expect(page.text).to match /The current time is [0-9]{2}:[0-9]{2}[ap]m./
     end
+
+    it 'prints 5 new paragraphs' do
+      visit '/index'
+      expect(page).to have_text 'This is a new paragraph! Number 1'
+      expect(page).to have_text 'This is a new paragraph! Number 2'
+      expect(page).to have_text 'This is a new paragraph! Number 3'
+      expect(page).to have_text 'This is a new paragraph! Number 4'
+      expect(page).to have_text 'This is a new paragraph! Number 5'
+    end
   end
 end
